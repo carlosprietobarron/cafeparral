562da94f73ddba7a35bddf1fc11df6156cd61be8
carlosprietobarron <carlosprietobarron@gmail.com>
2020-08-26 11:01:19 -0500

    Add a logo to the nav bar

--- src/NavBarDOM.js
+++ src/NavBarDOM.js
@@ -31,6 +31,17 @@ const navBarDom = (function nvbar() {
     anchor3.setAttribute('data-toggle', 'tab');
     anchor3.textContent = 'Conctact us';
     navTabs.appendChild(anchor3);
+
+    const logoCandas = document.createElement("a");
+    logoCandas.setAttribute("class","navbar-brand");
+    logoCandas.setAttribute("href","#");
+    NavBar.appendChild(logoCandas);
+    
+    const imgLogo = document.createElement("img");
+    imgLogo.setAttribute("src","./imag/logo.png");
+    logoCandas.appendChild(imgLogo);
+    
+
   }
 
   return { createNav };
